fce657c3b4d3a564f98cf07d8ec4943dc62e1747
Ricardo Maroquio <maroquio@gmail.com>
2024-11-06 16:52:26 -0300

    versao inicial de autenticacao e autorizacao

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,57 @@
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import api from "./axiosApi";
+import LoginForm from "./LoginForm";
+import FormButtons from './FormButtons';
+import Loading from './Loading';
+import handleChange from './handleChange';
+import parseErrors from './parseErrors';
+
 const Home = () => {
+    const [inputs, setInputs] = useState({});
+    const [errors, setErrors] = useState({});
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        setLoading(true);
+        api.post("/auth/entrar", inputs)
+            .then((response) => {                
+                if (response.status === 200) {
+                    navigate("/orders");
+                } else {
+                    console.log(response);
+                }
+            })
+            .catch((error) => {
+                if (error && error.response && error.response.data)
+                    setErrors(parseErrors(error.response.data));
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    }
+
+    function localHandleChange(event) {
+        handleChange(event, inputs, setInputs);
+    }
+
     return (
-        <h1>Página Principal</h1>
+        <>
+            <h1 className='mt-2'>Página Principal</h1>
+            <hr />
+            <h2>Entrar</h2>
+            <form onSubmit={handleSubmit} noValidate autoComplete="off">
+                <LoginForm handleChange={localHandleChange} errors={errors} inputs={inputs} />
+                <div>
+                    <button type="submit" className="btn btn-primary">
+                        Entrar
+                    </button>
+                </div>
+            </form>
+            {loading && <Loading />}
+        </>
     )
 }
 
---
+++ src/authService.js
@@ -0,0 +1,50 @@
+import api from "./axiosApi";
+import * as jwt from "jwt-decode"; // npm install jwt-decode
+
+export const login = async (email, senha) => {
+    let loggedIn = false;
+    await api
+        .postForm("auth/entrar", { "email": email, "senha": senha })
+        .then((response) => {
+            if (response.status === 200) {
+                if (response.data.token) {                    
+                    const jsonString = JSON.stringify(token);
+                    localStorage.setItem("token", jsonString);
+                    loggedIn = isAdmin();
+                }
+            } else {
+                console.log("Login error: " + response);
+            }
+        })
+        .catch((error) => {
+            console.log(error.message);
+        });
+    return loggedIn;
+};
+
+export const logout = () => {
+    localStorage.removeItem("token");
+};
+
+export const getToken = () => {
+    return JSON.parse(localStorage.getItem("token"));
+};
+
+export const isAdmin = () => {
+    const token = getToken();
+    if (token) {
+        const decoded = jwt(token);
+        return (decoded.perfil === 0);
+    } else {
+        return false;
+    }
+};
+
+export const authHeader = () => {
+    const token = getToken();
+    if (token) {
+        return { "x-access-token": token };
+    } else {
+        return {};
+    }
+};
