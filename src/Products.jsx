b4f983af51691ce26f799dce37e69c6bcba6adf1
Ricardo Maroquio <maroquio@gmail.com>
2024-11-13 14:09:00 -0300

    acertos em rotas administrativas que faltam o prefixo admin

--- src/Products.jsx
+++ src/Products.jsx
@@ -27,7 +27,7 @@ const Products = () => {
 
     const deleteProduct = (productId) => {
         setLoading(true);
-        api.postForm("excluir_produto", {"id_produto": productId})
+        api.postForm("admin/excluir_produto", {"id_produto": productId})
             .then(response => {
                 if (response.status === 204)
                     loadProducts();
