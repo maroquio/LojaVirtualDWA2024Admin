d6c8101958a151aeee3c35f99b6981139805025f
Ricardo Maroquio <maroquio@gmail.com>
2024-11-13 14:51:05 -0300

    exibindo html nas mensagens de erro

--- src/FormTextarea.jsx
+++ src/FormTextarea.jsx
@@ -6,7 +6,7 @@ const FormTextarea = ({ field, onChange, label, value, error, autofocus = false 
             <div className="form-floating">
                 <textarea className={`form-control ${error ? 'is-invalid' : 'is-valid'}`} placeholder=" " id={field} name={field} onChange={onChange} autoFocus={autofocus} style={{ height: '10rem' }} value={value || ""}></textarea>
                 <label htmlFor={field}>{label}</label>
-                {error && <div className="invalid-feedback">{error}</div>}
+                {error && <div className="invalid-feedback" dangerouslySetInnerHTML={{__html: error}} />}
             </div>
         </>
     );
--- src/ProductForm.jsx
+++ src/ProductForm.jsx
@@ -3,23 +3,23 @@ import CleaveInput from "./CleaveInput"
 import FormInput from "./FormInput"
 import FormTextarea from "./FormTextarea"
 
-const ProductForm = ({ handleChange, inputs, errors }) => {
+const ProductForm = ({ handleChange, inputs, errors, isNew }) => {
     return (
         <>
             <div className="row">
                 <div className="col-12 mb-3">
-                    <FormInput type="text" field="nome" label="Nome" value={inputs?.nome} onChange={handleChange} error={errors?.nome} autofocus={true} />
+                    <FormInput type="text" field="nome" label="Nome" value={isNew ? "" : inputs?.nome} onChange={handleChange} error={errors?.nome} autofocus={true} />
                 </div>
                 <div className="col-12 mb-3">
-                    <FormTextarea field="descricao" label="Descrição" value={inputs?.descricao} onChange={handleChange} error={errors?.descricao} />
+                    <FormTextarea field="descricao" label="Descrição" value={isNew ? "" : inputs?.descricao} onChange={handleChange} error={errors?.descricao} />
                 </div>
                 <div className="col-6 mb-3">
-                    <CleaveInput type="text" field="preco" label="Preço" value={inputs?.preco} onChange={handleChange} error={errors?.preco} options={{ numeral: true, numeralThousandsGroupStyle: 'thousand', prefix: 'R$ ', rawValueTrimPrefix: true, delimiter: '.', numeralDecimalMark: ',' }} />
+                    <CleaveInput type="text" field="preco" label="Preço" value={isNew ? "" : inputs?.preco} onChange={handleChange} error={errors?.preco} options={{ numeral: true, numeralThousandsGroupStyle: 'thousand', prefix: 'R$ ', rawValueTrimPrefix: true, delimiter: '.', numeralDecimalMark: ',' }} />
                 </div>
                 <div className="col-6 mb-3">
                     <CleaveInput type="text" field="estoque"
                         label="Estoque" onChange={handleChange}
-                        value={inputs.estoque} error={errors?.estoque}
+                        value={isNew ? "" : inputs.estoque} error={errors?.estoque}
                         options={{
                             numeral: true,
                             numeralPositiveOnly: true,
@@ -36,7 +36,12 @@ const ProductForm = ({ handleChange, inputs, errors }) => {
 ProductForm.propTypes = {
     handleChange: PropTypes.func.isRequired,
     inputs: PropTypes.object.isRequired,
-    errors: PropTypes.object
+    errors: PropTypes.object,
+    isNew: PropTypes.bool
+};
+
+ProductForm.propDefaults = {    
+    isNew: false
 };
 
 export default ProductForm;
