20c1f4a70bd84d7265d0296cf1f8b410ee1270b1
Ricardo Maroquio <maroquio@gmail.com>
2024-12-04 14:09:38 -0300

    alteracao do endereco base para producao

--- src/axiosApi.js
+++ src/axiosApi.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken } from './authService';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/',
+  baseURL: 'https://loja.maroquio.com/',
 });
 
 api.interceptors.request.use(
